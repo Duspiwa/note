7b7784a9e7ed65fbcb84bfd32e4b8917ddc320ff
Duspiwa <gpt1k7@hotmail.com>
2016-01-30 06:03:27 +0100

    changed index to show only current user notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
 
 
 	def index
-		@notes = Note.all.order("created_at DESC")
+		@notes = Note.where(user_id: current_user).order("created_at DESC")
 	end
 
 	def show
